1057133ce19b2e22b09c1ef3a52c52b5ddb29d3a
Mahdi <aparat1337@gmail.com>
2025-04-30 22:10:52 +0330

    Solve LongestCommonPrefix V1

---
+++ LongestCommonPrefix/Algorithm/Prefix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestCommonPrefix.Algorithm
+{
+    public class Prefix
+    {
+        public string LongestCommonPrefixV1(string[] strs)
+        {
+            if (strs.Length == 1)
+                return strs[0];
+            int arrayMinLenght = strs[0].Length;
+            foreach (var item in strs)
+            {
+                if (arrayMinLenght > item.Length)
+                    arrayMinLenght = item.Length;
+            }
+
+            int lastSameIndex = -1;
+            bool exit = false;
+            //each string
+            for (int i = 0; i < strs.Length - 1; i++)
+            {
+                if (exit)
+                    break;
+                //each char in string
+                for (int b = 0; b < arrayMinLenght; b++)
+                {
+                    if ((i - 1) >= 0 && strs[i - 1][b] != strs[i + 1][b])
+                    {
+                        if (arrayMinLenght == 1)
+                            lastSameIndex = lastSameIndex - 1;
+                        exit = true;
+                        break;
+                    }
+                    if (strs[i][b] == strs[i + 1][b])
+                    {
+                        lastSameIndex = b;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            if (lastSameIndex + 1 < 0)
+                return "";
+            return strs[0].Substring(0, lastSameIndex + 1);
+        }
+    }
+}
---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,7 @@
+﻿using LongestCommonPrefix.Algorithm;
+
+Prefix prefix = new Prefix();
+
+string[] array = { "flower", "flow", "flight" };
+
+Console.WriteLine(prefix.LongestCommonPrefixV1(array));
