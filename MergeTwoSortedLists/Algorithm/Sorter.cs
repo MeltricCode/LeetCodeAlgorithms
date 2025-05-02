f48348ed48bec63ffd9c49a22e3d9e2f9494cf83
Mahdi <aparat1337@gmail.com>
2025-05-02 17:12:52 +0330

    Add MergeTwoSortedList

---
+++ MergeTwoSortedLists/Algorithm/Sorter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeTwoSortedLists.Algorithm
+{
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Sorter
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2) // the performance is 0ms
+        {
+            ListNode dummy = new();
+            ListNode current = dummy;
+            while (list1 != null && list2 != null)
+            {
+                if(list1.val == list2.val)
+                {
+                    current.next = new(list1.val);
+                    current = current.next;
+                    current.next = new(list2.val);
+                    current = current.next;
+                    list1 = list1.next;
+                    list2 = list2.next;
+                }
+                else if (list1.val > list2.val)
+                {
+                    current.next = new(list2.val);
+                    current = current.next;
+                    list2 = list2.next;
+                }
+                else
+                {
+                    current.next = new(list1.val);
+                    current = current.next;
+                    list1 = list1.next;
+                }
+            }
+
+            current.next = list1 == null ? list2 : list1;
+            return dummy.next;
+        }
+    }
+}
---
+++ MergeTwoSortedLists/Program.cs
@@ -0,0 +1,21 @@
+﻿using MergeTwoSortedLists.Algorithm;
+
+// Problem url: https://leetcode.com/problems/merge-two-sorted-lists/
+
+Sorter sorter = new();
+
+var l1 = new ListNode(1);
+var l2 = new ListNode(2);
+var l3 = new ListNode(4);
+
+l1.next = l2;
+l2.next = l3;
+
+var l11 = new ListNode(1);
+var l22 = new ListNode(3);
+var l33 = new ListNode(5);
+
+l11.next = l22;
+l22.next = l33;
+
+Console.WriteLine(sorter.MergeTwoLists(l1, l11));
