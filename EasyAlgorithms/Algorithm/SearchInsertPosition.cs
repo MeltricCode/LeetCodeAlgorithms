f73a3a654984a08609cd945ed9fc41e41534381f
Mahdi <aparat1337@gmail.com>
2025-05-05 20:10:38 +0330

    Add SearchInsertPosition

---
+++ EasyAlgorithms/Algorithm/SearchInsertPosition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyAlgorithms.Algorithm
+{
+    public class SearchInsertPosition
+    {
+        public static int SearchInsert(int[] nums, int target) //the performance is 0ms
+        {
+            int low = 0;
+            int high = (nums.Length - 1);
+
+            while (low <= high)
+            {
+                int middle = low + (high - low) / 2;
+
+                if (nums[middle] == target)
+                {
+                    return middle;
+
+                }
+                else if (nums[middle] < target)
+                {
+                    low = middle + 1;
+                }
+                else
+                {
+                    high = middle - 1;
+                }
+            }
+            return low;
+        }
+    }
+}
---
+++ EasyAlgorithms/Program.cs
@@ -0,0 +1,3 @@
+﻿using EasyAlgorithms.Algorithm;
+
+Console.WriteLine(SearchInsertPosition.SearchInsert([1, 3, 5, 6], 2)); 
