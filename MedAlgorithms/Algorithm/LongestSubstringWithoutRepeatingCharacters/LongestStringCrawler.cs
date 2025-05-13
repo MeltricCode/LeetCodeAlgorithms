feb44ac56e4d01c28322e7255dfd70e706b1aa18
Mahdi <aparat1337@gmail.com>
2025-05-13 21:35:39 +0330

    Add (LongestSubstringWithoutRepeatingCharacters) 6ms

---
+++ MedAlgorithms/Algorithm/LongestSubstringWithoutRepeatingCharacters/LongestStringCrawler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedAlgorithms.Algorithm.LongestSubstringWithoutRepeatingCharacters
+{
+    //Probblem url: https://leetcode.com/problems/longest-substring-without-repeating-characters
+    public class LongestStringCrawler
+    {
+        public static int LengthOfLongestSubstring(string s) //The performance is 6ms
+        {
+            HashSet<char> charSet = new HashSet<char>();
+            int left = 0, maxLength = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                while (charSet.Contains(s[i]))
+                {
+                    charSet.Remove(s[left]);
+                    left++;
+                }
+
+                charSet.Add(s[i]);
+
+                maxLength = Math.Max(maxLength, i - left + 1);
+            }
+            return maxLength;
+        }
+    }
+}
--- MedAlgorithms/Program.cs
+++ MedAlgorithms/Program.cs
@@ -1,15 +1,4 @@
 ﻿using MedAlgorithms.Algorithm.AddTwoNumbers;
+using MedAlgorithms.Algorithm.LongestSubstringWithoutRepeatingCharacters;
 
-ListNode l1 = new(2);
-ListNode l12 = new(4);
-ListNode l13 = new(3);
-l1.next = l12;
-l1.next.next = l13;
-
-ListNode l2 = new(5);
-ListNode l22 = new(6);
-ListNode l23 = new(4);
-l2.next = l22;
-l2.next.next = l23;
-
-Console.WriteLine(ListNodeAddTwoReverseNumberV3.AddTwoNumbers(l1, l2));
+Console.WriteLine(LongestStringCrawler.LengthOfLongestSubstring("abcabcbb"));
